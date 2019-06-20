d681559a7e58c97618a9774ea9acbd7ffca242dc
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-06-20 06:13:48 -0300

    :lipstick: Styling services

--- components/services/Service.vue
+++ components/services/Service.vue
@@ -2,20 +2,29 @@
   .service
     .service__img
       picture
-        source(:srcset="props.src + '.webp'" type="image/webp")
-        img(:src="props.src +  '.png'")
+        source(
+          :srcset="`${props.src}--sm.webp 599w, ${props.src}--md.webp 1199w, ${props.src}--lg.webp 1200w`"
+          sizes="(max-width: 639px) 100%, (max-width: 1199px) 100%, 100%"
+          type="image/webp"
+          )
+        img(
+          :srcset="`${props.src}--sm.png 599w, ${props.src}--md.png 11998w, ${props.src}--lg.png 1200w`"
+          sizes="(max-width: 599px) 100%, (max-width: 1199px) 100%, 100%"
+          :src="`${props.src}--sm.png`"
+          )
+
     h4.service__name {{ props.name }}
     p.service__body {{ props.body }}
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .service {
-    border: solid 1px #333A;
+    // border: solid 1px #333A;
 
     &:hover {
-      color: #005c5e;
+      color: var(--color-main--light);
       &__img {
-        box-shadow: 0px 5px 10px 2px rgba(0,0,0,0.4);
+        box-shadow: 0px 5px 10px 2px var(--color-border);
       }
     }
 
@@ -34,6 +43,7 @@
     }
     &__body {
       margin: 1rem 0;
+      line-height: 1.5rem;
     }
   }
 </style>
--- components/services/Services.vue
+++ components/services/Services.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
   section.services
-    service(
-      v-for="(servicio, index) in servicios"
-      :key="index"
-      :name="servicio.name"
-      :body="servicio.body"
-      :src="servicio.src"
-      )
+    h4.services__title Nuestros servicios
+    .services__cards
+      service(
+        v-for="(servicio, index) in servicios"
+        :key="index"
+        :name="servicio.name"
+        :body="servicio.body"
+        :src="servicio.src"
+        )
 </template>
 
 <script>
@@ -19,58 +21,80 @@ export default {
       {
         name: 'Finales de obra',
         body: 'Una vez finalizada la obra, Mundo Litoral deja a punto el espacio para poder ser utilizado.',
-        src: 'img/service-1--lg'
+        src: 'img/service-1'
       },
       {
         name: 'Reparación de mobiliarios',
         body: 'Arreglo de sillas y pupitres. Realizamos tapizado y reparaciones en general.',
-        src: 'img/service-2--lg'
+        src: 'img/service-2'
       },
       {
         name: 'Armado y mantenimiento de vidrieras',
         body: 'Reposición de banner en locales y ploter en vidrieras. Se realizan cambios de gigantografías de publicidad.',
-        src: 'img/service-3--lg'
+        src: 'img/service-3'
       },
       {
         name: 'Saneamiento en cocinas de restaurantes.',
         body: 'Snack de Cines y Fast food.',
-        src: 'img/service-4--lg'
+        src: 'img/service-4'
       },
       {
         name: 'Acondicionamiento de salas de cine.',
         body: 'Tratamiento de telas, alfombras y butacas. Mantenimiento de sonido e iluminación.',
-        src: 'img/service-5--lg'
+        src: 'img/service-5'
       },
       {
         name: 'Cobertura de eventos',
         body: 'Pre y post acondicionamiento de espacio para eventos. Decoraciones integrales. Atención a invitados',
-        src: 'img/service-6--lg'
+        src: 'img/service-6'
       },
       {
         name: 'Reparaciones Generales',
         body: 'Pintura de espacios abiertos y cerrados. Permeabilización y cambios de luminarias en locales. Como reemplazo de alfombras y pisos',
-        src: 'img/service-7--lg'
+        src: 'img/service-7'
       },
       {
         name: 'Laverap',
         body: 'Atención a empresas, hoteles, centros de estética, bares, restaurant, equipos de clubes deportivos, clínicas, escuelas y particulares.',
-        src: 'img/service-8--lg'
+        src: 'img/service-8'
       },
       {
         name: 'Mantenimiento',
         body: 'En espacios abiertos y cerrados tales como locales, cines, colegios, bares, entre otros.',
-        src: 'img/service-9--lg'
+        src: 'img/service-9'
       }
     ]
   })
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .services {
-    margin: 5rem 5vw;
-    display: grid;
-    grid-gap: 2.5rem;
-    grid-template-columns: repeat(3, 1fr);
+    padding: 2.5rem 5vw;
+
+    &__cards {
+      display: grid;
+      grid-gap: 2.5rem;
+      grid-template-columns: 1fr;
+
+      @media (min-width: 600px) {
+        grid-template-columns: repeat(2, 1fr);
+      }
+
+      @media (min-width: 1200px) {
+        grid-template-columns: repeat(3, 1fr);
+      }
+    }
+
+    &__title {
+      font-size: 1.75rem;
+      margin-bottom: 1.5rem;
+
+      @media (min-width: 1200px) {
+        text-align: center;
+        color: var(--color-main--light);
+        margin: 2rem 0 3rem;
+      }
+    }
   }
 </style>
