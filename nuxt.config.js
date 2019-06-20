b7499a5889d1bdc49d05f36a14a5dd479f98980f
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-06-20 17:09:22 -0300

    Titulo y lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,10 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      lang: 'es'
+    },
+    title: 'Cooperativa Mundo Litoral',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
