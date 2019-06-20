f0f10f64e14dc2c5bd63dff7e204e32b037bf1ad
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-06-20 06:14:18 -0300

    Componentes de navbar y footer

---
+++ components/Footer.vue
@@ -0,0 +1,35 @@
+<template lang="pug" functional>
+  footer.footer
+    .footer__links
+      a(href="#inicio") Inicio
+      a(href="#servicios") Servicios
+      a(href="#nosotros") Nosotros
+      a(href="#clientes") Clientes
+      a(href="#contacto") Contacto
+</template>
+
+<style lang="scss">
+  .footer {
+    // padding: 2rem;
+    height: 25vh;
+    background-color: var(--color-main);
+    display: grid;
+    grid-template-areas: 'links';
+    grid-template-columns: 1fr;
+    color: white;
+
+    &__links {
+      grid-area: 'links';
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+
+      a {
+        text-decoration: none;
+        &:link { color: white; }
+        &:hover { color: white; }
+        &:visited { color: white; }
+      }
+    }
+  }
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  nav.navbar
+    .navbar__logo
+      picture
+        source(srcset="~/assets/img/logo.webp" type="image/webp")
+        img(src="~/assets/img/logo.jpg")
+</template>
+
+<style lang="scss">
+  .navbar {
+    height: 4rem;
+    padding: .5rem 1rem;
+
+    &__logo {
+      height: 100%;
+
+      * {
+        width: auto;
+        height: 100%;
+        object-fit: contain;
+      }
+    }
+  }
+</style>
