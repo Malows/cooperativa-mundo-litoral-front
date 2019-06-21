34bbf424647453b0e1b303d9ac98ff7f22222ef5
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-06-20 21:00:44 -0300

    Agrego el efecto hover a la card de caracteristica

--- components/cards/Card.vue
+++ components/cards/Card.vue
@@ -6,17 +6,22 @@
 
 <style lang="scss">
   .card {
-    border: solid 1px #333A;
+    $class: &;
+
+    border: solid 1px var(--color-divider);
     padding: 1rem;
     text-align: center;
 
     &--important {
-      box-shadow: 0px 5px 10px 2px rgba(0,0,0,0.4);
+      $shadow-color: rgba(34, 34, 34, 0.4);
+      box-shadow: 0px 5px 10px 2px $shadow-color;
       order: -1;
 
-      @media (min-width: 720px) {
-        order: 0;
+      &:hover {
+        box-shadow: 0 5px 10px 2px lighten($shadow-color, 40%);
       }
+
+      @media (min-width: 720px) { order: 0; }
     }
 
     &__title {
@@ -30,9 +35,13 @@
       margin-top: 1rem;
       font-weight: 100;
 
-      @media (max-width: 719px) {
-        margin: 1rem 0;
-      }
+      @media (max-width: 719px) { margin: 1rem 0; }
+    }
+
+    &:hover {
+      border-color: var(--color-border);
+
+      #{$class}__title { color: var(--color-main--lighter); }
     }
   }
 </style>
