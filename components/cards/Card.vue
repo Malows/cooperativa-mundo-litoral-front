a6cff7738625138811df7d39d6703708d77b6de1
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-07-04 01:59:00 -0300

    Le saco la sombra gratis a l card importante

--- components/cards/Card.vue
+++ components/cards/Card.vue
@@ -13,13 +13,12 @@
     text-align: center;
 
     &--important {
-      $shadow-color: rgba(34, 34, 34, 0.4);
-      box-shadow: 0px 5px 10px 2px $shadow-color;
       order: -1;
 
-      &:hover {
-        box-shadow: 0 5px 10px 2px lighten($shadow-color, 40%);
-      }
+      // &:hover {
+      //   $shadow-color: rgba(34, 34, 34, 0.4);
+      //   box-shadow: 0 5px 10px 2px lighten($shadow-color, 40%);
+      // }
 
       @media (min-width: 720px) { order: 0; }
     }
@@ -40,6 +39,8 @@
 
     &:hover {
       border-color: var(--color-border);
+      $shadow-color: rgba(34, 34, 34, 0.4);
+      box-shadow: 0 5px 10px 2px lighten($shadow-color, 40%);
 
       #{$class}__title { color: var(--color-main--lighter); }
     }
