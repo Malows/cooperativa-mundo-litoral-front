22d16e62b72e0d8fb2b2d1dd73bde2e143788b48
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-07-04 02:02:07 -0300

    fuente en html

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   main
-    navbar#inicio
+    navbar#inicio(:width="width")
 
     carousel(:width="width" :support-webp="supportWebp")
 
@@ -90,35 +90,5 @@ export default {
         top: 0;
       }
     }
-  }
-
-  .container {
-    margin: 0 auto;
-    min-height: 70vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    background-color: #2222;
-  }
-
-  .title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-      'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 3rem;
-    letter-spacing: 1px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
   }
 </style>
