d720b78730418671d0011895fb76d5a6c71dc8af
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-07-04 01:59:10 -0300

    Menu de navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,13 +4,39 @@
       picture
         source(srcset="~/assets/img/logo.webp" type="image/webp" alt="brand logo")
         img(src="~/assets/img/logo.jpg" alt="brand logo")
+
+    navbar-menu(v-if="this.width < 600" :links="links")
+    .navbar__items(v-else)
+      a.navbar__link(v-for="link in links" :key="value" :href="link.value") {{ link.label }}
 </template>
+
+<script>
+import NavbarMenu from './NavbarMenu'
+
+export default {
+  components: { NavbarMenu },
+
+  props: { width: { type: Number, default: 0 } },
+
+  data: () => ({
+    links: [
+      { label: 'Inicio', value: '#inicio' },
+      { label: 'Servicios', value: '#servicios' },
+      { label: 'Nosotros', value: '#nosotros' },
+      { label: 'Clientes', value: '#clientes' },
+      { label: 'Contacto', value: '#contacto' }
+    ]
+  })
+}
+</script>
 
 <style lang="scss">
   .navbar {
     height: 4rem;
     padding: .5rem 1rem;
     background-color: #fff;
+    display: flex;
+    justify-content: space-between;
 
     @media (min-width: 600px) { height: 10vh; }
 
@@ -22,6 +48,21 @@
         height: 100%;
         object-fit: contain;
       }
+    }
+
+    &__items {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    &__link {
+      padding: 1rem;
+      text-decoration: none;
+      cursor: pointer;
+      @each $op in link, hover, visited, active {
+          &:#{$op} { color: var(--color-divider); }
+        }
     }
   }
 </style>
---
+++ components/NavbarMenu.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  .navbar-menu
+    button.navbar-menu__button(@click="show = !show")
+      fa(:icon="iconValue")
+    .navbar-menu__items(v-show="show")
+      a.navbar__link(
+        v-for="link in links"
+        :key="`burguer-${link.value}`"
+        :href="link.value"
+        @click="show = false") {{ link.label }}
+
+</template>
+
+<script>
+export default {
+  props: { links: { type: Array, default: () => [] } },
+
+  data: () => ({ show: false }),
+
+  computed: {
+    iconValue() {
+      return this.show
+        ? ['fas', 'times']
+        : ['fas', 'bars']
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.navbar-menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__button {
+    border: none;
+    font-size: 1.5rem;
+  }
+
+  &__items {
+    position: absolute;
+    top: 4rem;
+    left: 0;
+    width: 100vw;
+    z-index: 999;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
